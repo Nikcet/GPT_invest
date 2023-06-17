4aeb3514d76141c45507f0f25d5399af7cf2ddc4
Nikcet <mstislav.kr@yandex.ru>
2023-06-17 14:07:35 +0300

    take storage

--- src/features/rootSlice.ts
+++ src/features/rootSlice.ts
@@ -1,35 +1,19 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-interface IMatch {
-  firstCommand: string;
-//   secondCommand: string;
-//   isRunning: boolean;
-//   isPaused: boolean;
-//   period: number;
-//   seconds: number;
-//   minutes: number;
-//   firstCommandScore: number;
-//   secondCommandScore: number;
+interface IAnswer {
+  answer: string;
 }
 
-const initialState: IMatch = {
-  firstCommand: 'Команда 1',
-//   secondCommand: 'Команда 2',
-//   isRunning: false,
-//   isPaused: false,
-//   period: 1,
-//   seconds: 0,
-//   minutes: 12,
-//   firstCommandScore: 0,
-//   secondCommandScore: 0,
+const initialState: IAnswer = {
+  answer: '',
 };
 
 const rootSlice = createSlice({
   name: 'Match',
   initialState,
   reducers: {
-    setFirstName(state, action: PayloadAction<string>) {
-      state.firstCommand = action.payload;
+    setAnswer(state, action: PayloadAction<string>) {
+      state.answer = action.payload;
     },
   }
 });
