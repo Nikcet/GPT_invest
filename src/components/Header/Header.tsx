784b0e498942205211635ef62de11a6d6e55d0d3
Nikcet <mstislav.kr@yandex.ru>
2023-06-20 16:21:25 +0300

    init new component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import { Container, Box, CircularProgress, Typography, Card, CardContent } from '@mui/material';
+
+// interface IProps {
+//     response: string | undefined
+// }
+
+function Header() {
+
+    return (
+        <Container sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            
+        </Container>
+    );
+}
+
+export default Header;
